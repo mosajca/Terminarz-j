5a90c6a06cf367023cf1bf27b774fb6e854b4b19
Marcin Osajca <marcin9608@gmail.com>
2019-01-14 10:50:27 +0100

    add buttons actions

--- src/main/java/schedule/gui/Layout.java
+++ src/main/java/schedule/gui/Layout.java
@@ -7,6 +7,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -28,7 +29,9 @@ public class Layout extends BorderPane {
 
     private Database database;
     private EventWindow eventWindow;
-    private List<AnchorPane> anchorPanes = new ArrayList<>();
+    private OffsetDateTime current;
+    private List<AnchorPane> weekAnchorPanes = new ArrayList<>();
+    private boolean week = true;
 
     public Layout(Database database) {
         this.database = database;
@@ -58,7 +61,10 @@ public class Layout extends BorderPane {
             toggleButton.setToggleGroup(toggleGroup);
             left.getChildren().add(toggleButton);
         }
-        setThisWeekAction(left.getChildren().get(2));
+        setDayAction(left.getChildren().get(0), OffsetDateTime::now);
+        setDayAction(left.getChildren().get(1), () -> OffsetDateTime.now().plusDays(1));
+        setWeekAction(left.getChildren().get(2), OffsetDateTime::now);
+        setWeekAction(left.getChildren().get(3), () -> OffsetDateTime.now().plusDays(7));
         return left;
     }
 
@@ -74,7 +80,7 @@ public class Layout extends BorderPane {
     private ScrollPane createCenterCenter() {
         HBox hbox = new HBox();
         AnchorPane time = new AnchorPane();
-        anchorPanes.add(time);
+        weekAnchorPanes.add(time);
         hbox.getChildren().add(time);
         for (int i = 0; i < 24; ++i) {
             Label label = new Label(String.format("%02d:00", i));
@@ -89,7 +95,8 @@ public class Layout extends BorderPane {
             anchorPane.getStyleClass().add("anchor-pane");
             anchorPane.prefWidthProperty().bind(hbox.widthProperty().divide(8));
             HBox.setHgrow(anchorPane, Priority.ALWAYS);
-            anchorPanes.add(anchorPane);
+            anchorPane.managedProperty().bind(anchorPane.visibleProperty());
+            weekAnchorPanes.add(anchorPane);
             hbox.getChildren().add(anchorPane);
         }
         ScrollPane scrollPane = new ScrollPane(hbox);
@@ -107,6 +114,34 @@ public class Layout extends BorderPane {
         Button next = new Button("->");
         HBox.setHgrow(next, Priority.ALWAYS);
         bottom.getChildren().addAll(prev, next);
+        prev.setOnAction(event -> {
+            if (current == null) return;
+            try {
+                weekAnchorPanes.stream().skip(1).forEach(a -> a.setVisible(week));
+                current = current.minusDays(week ? 7 : 1);
+                if (week) {
+                    showEvents(database.selectEventWhereWeek(current));
+                } else {
+                    weekAnchorPanes.get(current.getDayOfWeek().getValue()).setVisible(true);
+                    showEvents(database.selectEventWhereDay(current));
+                }
+            } catch (SQLException e) {
+            }
+        });
+        next.setOnAction(event -> {
+            if (current == null) return;
+            try {
+                weekAnchorPanes.stream().skip(1).forEach(a -> a.setVisible(week));
+                current = current.plusDays(week ? 7 : 1);
+                if (week) {
+                    showEvents(database.selectEventWhereWeek(current));
+                } else {
+                    weekAnchorPanes.get(current.getDayOfWeek().getValue()).setVisible(true);
+                    showEvents(database.selectEventWhereDay(current));
+                }
+            } catch (SQLException e) {
+            }
+        });
         return bottom;
     }
 
@@ -115,15 +150,24 @@ public class Layout extends BorderPane {
         top.getStyleClass().add("center-top");
         Region region = new Region();
         HBox.setHgrow(region, Priority.ALWAYS);
-        region.maxWidthProperty().bind(anchorPanes.get(0).widthProperty());
-        region.prefWidthProperty().bind(anchorPanes.get(0).widthProperty());
+        region.maxWidthProperty().bind(weekAnchorPanes.get(0).widthProperty());
+        region.prefWidthProperty().bind(weekAnchorPanes.get(0).widthProperty());
         top.getChildren().add(region);
         int i = 1;
         for (String s : Arrays.asList("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")) {
             Button button = new Button(s);
+            button.setUserData(i);
+            button.setOnAction(event -> {
+                week = false;
+                current = current.plusDays((int) button.getUserData() - current.getDayOfWeek().getValue());
+                weekAnchorPanes.stream().skip(1).forEach(a -> a.setVisible(false));
+                weekAnchorPanes.get((int) button.getUserData()).setVisible(true);
+            });
             HBox.setHgrow(button, Priority.ALWAYS);
-            button.maxWidthProperty().bind(anchorPanes.get(i).widthProperty());
-            button.prefWidthProperty().bind(anchorPanes.get(i++).widthProperty());
+            button.managedProperty().bind(button.visibleProperty());
+            button.visibleProperty().bind(weekAnchorPanes.get(i).visibleProperty());
+            button.maxWidthProperty().bind(weekAnchorPanes.get(i).widthProperty());
+            button.prefWidthProperty().bind(weekAnchorPanes.get(i++).widthProperty());
             top.getChildren().add(button);
         }
         return top;
@@ -141,18 +185,36 @@ public class Layout extends BorderPane {
         return label;
     }
 
-    private void setThisWeekAction(Node node) {
+    private void setWeekAction(Node node, Supplier<OffsetDateTime> dateTimeSupplier) {
         node.setOnMouseClicked(event -> {
-            anchorPanes.stream().skip(1).forEach(a -> a.getChildren().clear());
+            weekAnchorPanes.stream().skip(1).forEach(a -> a.setVisible(true));
+            week = true;
             try {
-                List<Event> events = database.selectEventWhereWeek(OffsetDateTime.now());
-                for (Event e : events) {
-                    anchorPanes.get(e.getStartDateTime().getDayOfWeek().getValue())
-                            .getChildren().add(createEventLabel(e));
-                }
+                current = dateTimeSupplier.get();
+                showEvents(database.selectEventWhereWeek(current));
             } catch (SQLException e) {
             }
         });
+    }
+
+    private void setDayAction(Node node, Supplier<OffsetDateTime> dateTimeSupplier) {
+        node.setOnMouseClicked(event -> {
+            weekAnchorPanes.stream().skip(1).forEach(a -> a.setVisible(false));
+            week = false;
+            try {
+                current = dateTimeSupplier.get();
+                weekAnchorPanes.get(current.getDayOfWeek().getValue()).setVisible(true);
+                showEvents(database.selectEventWhereDay(current));
+            } catch (SQLException e) {
+            }
+        });
+    }
+
+    private void showEvents(List<Event> events) {
+        weekAnchorPanes.stream().skip(1).forEach(a -> a.getChildren().clear());
+        for (Event e : events) {
+            weekAnchorPanes.get(e.getStartDateTime().getDayOfWeek().getValue()).getChildren().add(createEventLabel(e));
+        }
     }
 
 }
